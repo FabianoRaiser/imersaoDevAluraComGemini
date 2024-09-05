8261a83e813c3b13a05e29e168d7a77bc0048aef
Fabiano Raiser <fabiano.raiser16@gmail.com>
2024-09-04 21:45:48 -0300

    feat: :construction: Starting the development of a dynamic generative data

---
+++ dados.js
@@ -0,0 +1,63 @@
+let dados = [
+    {
+        nome: "Ayrton Senna da Silva",
+        descricao: "Considerado um dos maiores pilotos de Fórmula 1 de todos os tempos, conquistou três títulos mundiais.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Ayrton_Senna",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Ayrton_Senna_1993.jpg/220px-Ayrton_Senna_1993.jpg"
+      },
+      {
+        nome: "Emerson Fittipaldi",
+        descricao: "Bicampeão mundial de Fórmula 1, também se destacou nas corridas de IndyCar.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Emerson_Fittipaldi",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Emerson_Fittipaldi_1972.jpg/220px-Emerson_Fittipaldi_1972.jpg"
+      },
+      {
+        nome: "Nelson Piquet",
+        descricao: "Tricampeão mundial de Fórmula 1, conhecido por seu estilo agressivo de pilotagem.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Nelson_Piquet",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Nelson_Piquet_1983.jpg/220px-Nelson_Piquet_1983.jpg"
+      },
+      {
+        nome: "Felipe Massa",
+        descricao: "Venceu 11 corridas na Fórmula 1, sendo um dos pilotos mais populares da categoria.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Felipe_Massa",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Felipe_Massa_2013.jpg/220px-Felipe_Massa_2013.jpg"
+      },
+      {
+        nome: "Rubens Barrichello",
+        descricao: "Um dos pilotos com mais corridas disputadas na Fórmula 1.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Rubens_Barrichello",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Rubens_Barrichello_2011.jpg/220px-Rubens_Barrichello_2011.jpg"
+      },
+      {
+        nome: "Tony Kanaan",
+        descricao: "Campeão da Indy 500, um dos pilotos mais bem sucedidos da IndyCar.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Tony_Kanaan",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Tony_Kanaan_2013.jpg/220px-Tony_Kanaan_2013.jpg"
+      },
+      {
+        nome: "Pietro Fittipaldi",
+        descricao: "Neto de Emerson Fittipaldi, tem se destacado nas categorias de base do automobilismo.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Pietro_Fittipaldi",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Pietro_Fittipaldi_2020.jpg/220px-Pietro_Fittipaldi_2020.jpg"
+      },
+      {
+        nome: "Ingo Hoffmann",
+        descricao: "Um dos maiores pilotos brasileiros de turismo, com diversos títulos na Stock Car.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Ingo_Hoffmann",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Ingo_Hoffmann_2013.jpg/220px-Ingo_Hoffmann_2013.jpg"
+      },
+      {
+        nome: "Alceu Feldmann",
+        descricao: "Pioneiro do automobilismo brasileiro, com diversas vitórias em provas nacionais e internacionais.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Alceu_Feldmann",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Alceu_Feldmann_1958.jpg/220px-Alceu_Feldmann_1958.jpg"
+      },
+      {
+        nome: "Tarso Marques",
+        descricao: "Piloto brasileiro com passagens pela Fórmula 1 e IndyCar.",
+        link_wikipedia: "https://pt.wikipedia.org/wiki/Tarso_Marques",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Tarso_Marques_2005.jpg/220px-Tarso_Marques_2005.jpg"
+      }
+
+]
